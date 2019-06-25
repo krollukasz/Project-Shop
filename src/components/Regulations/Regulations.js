8faea9cd472edf1977c0a5a4f3692a85ae88382e
Łukasz Król <lionel@o2.pl>
2019-06-25 16:09:42 +0200

    Add regulations component

---
+++ src/components/Regulations/Regulations.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+// Import styles
+import "./Regulations.scss";
+
+const Regulations = () => (
+  <div className="regulations">
+    <h1 className="regulations-header">Regulations</h1>
+    <p className="regulations-paragraph">
+      1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+      lobortis elementum nibh tellus molestie nunc. Amet venenatis urna cursus
+      eget nunc scelerisque viverra. Odio euismod lacinia at quis risus.
+      Lorem mollis aliquam ut porttitor. Semper eget duis at tellus at.
+      Dictum varius duis at consectetur lorem donec. Id porta nibh
+      venenatis cras sed. Diam donec adipiscing tristique risus nec feugiat
+      in fermentum posuere. Et molestie ac feugiat sed lectus vestibulum.
+      Cursus risus at ultrices mi tempus. Ut tortor pretium viverra
+      suspendisse potenti nullam. Vitae proin sagittis nisl rhoncus mattis rhoncus
+      urna. Ut aliquam purus sit amet. Vulputate enim nulla aliquet
+      porttitor lacus luctus accumsan tortor. Gravida quis blandit turpis cursus
+      in hac habitasse. Enim nunc faucibus a pellentesque sit. Morbi
+      tincidunt ornare massa eget egestas purus viverra. Lacinia quis vel
+      eros donec ac odio tempor. Quis auctor elit sed vulputate mi.
+    </p>
+
+    <p className="regulations-paragraph">
+      2. In hac habitasse platea dictumst quisque sagittis. Tristique magna sit
+      amet purus gravida quis blandit. Vitae tortor condimentum lacinia quis.
+      Tristique magna sit amet purus gravida quis blandit turpis. Lacus
+      vel facilisis volutpat est velit egestas dui id ornare. Ultricies
+      mi eget mauris pharetra et ultrices neque. Vitae et leo
+      duis ut diam quam nulla. Tristique risus nec feugiat in
+      fermentum posuere urna nec. Elit pellentesque habitant morbi tristique senectus
+      et netus et malesuada. Ornare lectus sit amet est placerat.
+      Lectus nulla at volutpat diam ut venenatis tellus in. Lectus
+      proin nibh nisl condimentum. Accumsan lacus vel facilisis volutpat est
+      velit egestas. Sed viverra ipsum nunc aliquet bibendum enim facilisis
+      gravida neque. Dis parturient montes nascetur ridiculus mus mauris vitae.
+      Egestas pretium aenean pharetra magna ac placerat vestibulum. Tortor posuere
+      ac ut consequat. Ultrices neque ornare aenean euismod elementum nisi
+      quis eleifend quam. Amet consectetur adipiscing elit duis tristique sollicitudin
+      nibh sit amet. Diam sollicitudin tempor id eu nisl.
+    </p>
+
+    <p className="regulations-paragraph">
+      3. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam.
+      Vitae tortor condimentum lacinia quis vel eros donec ac odio.
+      Suspendisse sed nisi lacus sed. Faucibus turpis in eu mi
+      bibendum neque. Amet massa vitae tortor condimentum. Tristique senectus et
+      netus et malesuada fames ac. Egestas sed tempus urna et
+      pharetra pharetra massa massa. Adipiscing elit ut aliquam purus sit
+      amet luctus. Nisl purus in mollis nunc sed id semper.
+      Eleifend donec pretium vulputate sapien nec. Id diam vel quam
+      elementum pulvinar. Semper viverra nam libero justo. Cursus vitae congue
+      mauris rhoncus aenean vel elit. Nec dui nunc mattis enim
+      ut tellus. Justo eget magna fermentum iaculis. Pharetra magna ac
+      placerat vestibulum lectus. Enim diam vulputate ut pharetra. Morbi tincidunt
+      augue interdum velit euismod. Bibendum neque egestas congue quisque egestas.
+      Dignissim cras tincidunt lobortis feugiat.
+    </p>
+
+    <p className="regulations-paragraph">
+      4. Dui vivamus arcu felis bibendum. Nulla aliquet enim tortor at.
+      Porttitor lacus luctus accumsan tortor posuere ac ut. Facilisi nullam
+      vehicula ipsum a arcu cursus vitae. Sed vulputate mi sit
+      amet mauris commodo. Lectus mauris ultrices eros in. Morbi non
+      arcu risus quis varius quam quisque. In iaculis nunc sed
+      augue. Maecenas pharetra convallis posuere morbi leo urna. Congue eu
+      consequat ac felis donec et odio pellentesque. Duis at consectetur
+      lorem donec massa. Dignissim cras tincidunt lobortis feugiat vivamus at
+      augue. Duis at consectetur lorem donec massa sapien faucibus. Amet
+      commodo nulla facilisi nullam vehicula ipsum a arcu cursus.
+    </p>
+
+    <p className="regulations-paragraph">
+      5. Condimentum id venenatis a condimentum. Facilisi morbi tempus iaculis urna
+      id volutpat lacus laoreet. Congue quisque egestas diam in arcu
+      cursus euismod quis viverra. Et pharetra pharetra massa massa ultricies
+      mi. Mattis molestie a iaculis at erat pellentesque adipiscing commodo
+      elit. Sit amet justo donec enim diam vulputate. Ullamcorper dignissim
+      cras tincidunt lobortis feugiat vivamus at. Morbi tempus iaculis urna
+      id volutpat lacus laoreet non curabitur. Risus nullam eget felis
+      eget nunc lobortis mattis aliquam faucibus. Venenatis lectus magna fringilla
+      urna porttitor rhoncus dolor purus. Tellus integer feugiat scelerisque varius
+      morbi enim. Parturient montes nascetur ridiculus mus mauris vitae. Massa
+      tempor nec feugiat nisl. Sagittis nisl rhoncus mattis rhoncus urna
+      neque viverra justo. Euismod elementum nisi quis eleifend quam adipiscing
+      vitae proin sagittis. Adipiscing at in tellus integer feugiat scelerisque
+      varius morbi.
+    </p>
+  </div>
+);
+
+export default Regulations;
