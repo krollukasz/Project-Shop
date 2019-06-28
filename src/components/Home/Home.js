ec24d7935e3a23968b24ba9dd60a432881afc6ff
Łukasz Król <lionel@o2.pl>
2019-06-28 22:12:14 +0200

    Add Home component with sample text

---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// Import styles
+// import "./Home.scss";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="home">
+        Sample text
+      </div>
+    )
+  }
+}
+
+export default Home;
