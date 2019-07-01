eb0ac5e4462a11f78324b628a57d646d4c8e2e22
Łukasz Król <lionel@o2.pl>
2019-07-01 21:08:43 +0200

    Change component state, fix bug with render product

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -9,24 +9,40 @@ import { Product } from './Product';
 export class ProductList extends React.Component {
   constructor(props) {
     super(props);
-    this.state.products;
+    this.state = {
+      currentPage: 0,
+      currentProducts: [0, 6]
+    };
+    this.elements = this.props.sorted.length;
+    this.pageSize = 6;
+    this.pagesCount = Math.ceil(this.elements / this.pageSize);
+  }
+
+  handleClick(e, index) {
+    e.preventDefault();
+    this.setState({
+      currentPage: index,
+      currentProducts: [index * this.pageSize, (index + 1) * this.pageSize]
+    });
   }
 
   render() {
     return(
       <div className="productList">
         {
-          this.state.products.map((product, i) => (
-            <Product
-              key={i}
-              index={i}
-              name={product.name}
-              description={product.description}
-              price={product.price}
-              productInfo={product.productInfo}
-              image={product.image}
-            />
-          ))
+          (this.props.sorted.slice(this.state.currentProducts[0], this.state.currentProducts[1])).map((product, i) => {
+            return (
+              <Product
+                key={i}
+                id={product.id}
+                name={product.name}
+                description={product.description}
+                price={product.price}
+                productInfo={product.productInfo}
+                image={product.image}
+              />
+            )
+          })
         }
       </div>
     );
