2b646a8fdf815af2a1c3e3d6941f9dcd4712c6de
Łukasz Król <lionel@o2.pl>
2019-07-02 21:38:45 +0200

    Add sorting function and component styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,21 +1,50 @@
 import React from 'react';
+import { ProductList } from '../ProductList/ProductList';
+import { sort } from './sort';
+import { connect } from 'react-redux';
 
 // Import styles
-// import "./Home.scss";
+import "./Home.scss";
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      "products": this.props.products
+    };
+  }
+
+  sort(event) {
+    // event.preventDefault();
+    this.setState({
+      "products": this.props.products.sort(sort(event.target.dataset.prop, event.target.dataset.order))
+    });
   }
 
   render() {
     return (
       <div className="home">
-        Sample text
+        <div className="home-sort col-sm-12 col-md-4 col-lg-3">
+          <p className="home-sort-header">
+            Sort:
+          </p>
+          <ul className="home-sort-list">
+            <li className="home-sort-list-item" data-prop="name" data-order="asc" onClick={event => this.sort(event)}>Name: A-Z</li>
+            <li className="home-sort-list-item" data-prop="name" data-order="desc" onClick={event => this.sort(event)}>Name: Z-A</li>
+            <li className="home-sort-list-item" data-prop="price" data-order="asc" onClick={event => this.sort(event)}>Price: ascending</li>
+            <li className="home-sort-list-item" data-prop="price" data-order="desc" onClick={event => this.sort(event)}>Price: descending</li>
+          </ul>
+        </div>
+        <ProductList sorted={this.state} />
       </div>
     )
   }
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return {
+    products: state.products
+  }
+}
+
+export default connect(mapStateToProps)(Home);
