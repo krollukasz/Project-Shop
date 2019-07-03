2f1e9d87ce809befdc1de4f4cebed6d708bd7771
Łukasz Król <lionel@o2.pl>
2019-07-03 14:21:13 +0200

    Add pagination and component styles

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Product } from './Product';
-// import { Products } from './Products';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 // Import styles
-// import "./ProductList.scss";
+import "./ProductList.scss";
 
 export class ProductList extends React.Component {
   constructor(props) {
@@ -27,13 +27,16 @@ export class ProductList extends React.Component {
   }
 
   render() {
+    const { currentPage } = this.state;
     return (
-      <div className="product-list">
+      <div className="product-list col-sm-12 col-md-8 col-lg-9">
+        <div className="product-list-wrapper">
         {
           (this.props.sorted.products.slice(this.state.currentProducts[0], this.state.currentProducts[1])).map((product, i) => {
             return (
               <Product
                 key={i}
+                index={i}
                 id={product.id}
                 name={product.name}
                 description={product.description}
@@ -44,6 +47,34 @@ export class ProductList extends React.Component {
             );
           })
         }
+        </div>
+        <div className="pagination-wrapper">
+          <Pagination className="pagination-main" aria-label="Page Navigation">
+            <PaginationItem disabled={currentPage <= 0}>
+              <PaginationLink
+                onClick={e => this.handleClick(e, currentPage - 1)}
+                previous
+                href="#"
+              />
+            </PaginationItem>
+
+            {[...Array(this.pagesCount)].map((page, i) => (
+              <PaginationItem active={i === currentPage} key={i}>
+                <PaginationLink onClick={e => this.handleClick(e, i)} href="#">
+                  {i + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+
+            <PaginationItem disabled={currentPage >= this.pagesCount - 1}>
+              <PaginationLink
+                onClick={e => this.handleClick(e, currentPage + 1)}
+                next
+                href="#"
+              />
+            </PaginationItem>
+          </Pagination>
+        </div>
       </div>
     );
   };
